d8d0703bfac64a8aea06133907a1216a3eab3830
Kandy-Dev223 <eokon5914@gmail.com>
2025-03-03 02:58:46 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# gideon123" 
+"# gideon123" 
---
+++ webzy - Copy (2)/webzy - Copy/gide.js
@@ -0,0 +1,5 @@
+document.getElementById("language-select").addEventListener("change", function() {
+    let selectedLang = this.value;
+    alert("Language changed to: " + selectedLang);
+    // Implement actual translation logic if needed.
+});
